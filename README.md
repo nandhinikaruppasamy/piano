58151a771b09a08b52564589c7cb6999954dd69e
Nandhini <111649724+nandhinikaruppasamy@users.noreply.github.com>
2024-08-14 19:14:02 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # piano
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+IMAGE:
+
+
+![image](https://github.com/nandhinikaruppasamy/piano/assets/111649724/9355da24-3a90-4fb0-97fd-6e6ac36d7ce9)
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\karru\\Downloads\\flutter_windows_3.10.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\karru\\AndroidStudioProjects\\piano" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\karru\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\karru\\AndroidStudioProjects\\piano"
+  "FLUTTER_ROOT=C:\\Users\\karru\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\karru\\AndroidStudioProjects\\piano\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\karru\\AndroidStudioProjects\\piano"
+  "FLUTTER_TARGET=C:\\Users\\karru\\AndroidStudioProjects\\piano\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\karru\\AndroidStudioProjects\\piano\\.dart_tool\\package_config.json"
+)
